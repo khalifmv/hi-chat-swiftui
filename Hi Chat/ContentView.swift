8fbfb621fb1defbca8ffa2a41441a6c30639e370
Khalif <akhmadklf@gmail.com>
2021-08-13 14:09:55 +0700

    Initial Commit

---
+++ Hi Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hi Chat
+//
+//  Created by akhmad khalif on 13/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hi Chat/Hi_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hi_ChatApp.swift
+//  Hi Chat
+//
+//  Created by akhmad khalif on 13/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Hi_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
